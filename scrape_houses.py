d70d6576f9f331bde92ec78e10035a5e1b98f3d7
Jesús Ojeda Latronico <88132973+Ojedalatronico@users.noreply.github.com>
2025-05-29 19:21:41 +0200

    fix: add type ignore comment for mypy compatibility in scraper script

--- scrape_houses.py
+++ scrape_houses.py
@@ -2,7 +2,7 @@
 """Script to run the Tibia houses scraper."""
 
 import asyncio
-from tibiahouses.main import main
+from tibiahouses.main import main  # type: ignore
 
 if __name__ == "__main__":
     asyncio.run(main())
